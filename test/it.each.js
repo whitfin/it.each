c66891b38adfcdf3f3b2eb26cd98a39b53eea1c5
Isaac Whitfield <iw@whitfin.io>
2018-07-14 13:46:21 -0700

    Neaten implementation of it.each.skip

--- test/it.each.js
+++ test/it.each.js
@@ -1,12 +1,11 @@
 require('should');
 
-describe('it.each with testPerIteration: true', function(){
-
+describe('it.each with testPerIteration: true', function () {
     require('../index')({
         testPerIteration: true
     });
 
-    it.each([1], 'should be able to access the Mocha context', function(/* element */){
+    it.each([1], 'should be able to access the Mocha context', function (_element) {
         this.slow(1000);
         this.timeout(1000);
 
@@ -22,82 +21,74 @@ describe('it.each with testPerIteration: true', function(){
         this._runnable.parent._timeout.should.eql(2000);
     });
 
-    it.each(1, 'should place non-array values into an array', function(element){
+    it.each(1, 'should place non-array values into an array', function (element) {
         element.should.be.ok;
         element.should.eql(1);
     });
 
-    describe('executes', function(){
-
+    describe('executes', function () {
         this.slow(1000);
 
-        it.each([1], 'synchronously', function(/* element */){
+        it.each([1], 'synchronously', function (_element) {
             this._runnable.title.length.should.be.greaterThan(1);
         });
 
-        it.each([1], 'asynchronously', function(element, next){
+        it.each([1], 'asynchronously', function (_element, next) {
             this._runnable.title.length.should.be.greaterThan(1);
             setTimeout(next, 200);
         });
-
     });
 
-    describe('looping over arrays', function(){
-
+    describe('looping over arrays', function () {
         var count = 0;
 
-        it.each([1], 'should be able to access array elements', function(element){
+        it.each([1], 'should be able to access array elements', function (element) {
             element.should.be.ok;
             element.should.eql(1);
         });
 
-        it.each([1,2,3], 'should loop over each element in an array', function(element){
+        it.each([1,2,3], 'should loop over each element in an array', function (element) {
             element.should.be.ok;
             element.should.eql(++count);
         });
 
-        it('should increment a count', function(){
+        it('should increment a count', function () {
             count.should.be.ok;
             count.should.eql(3);
         });
-
     });
 
-    describe('titles', function(){
-
-        it.each([1], 'should contain the current index: %s', ['x'], function(/* element */){
+    describe('titles', function () {
+        it.each([1], 'should contain the current index: %s', ['x'], function (_element) {
             this._runnable.should.be.ok;
             this._runnable.title.should.be.ok;
             this._runnable.title.should.endWith('0');
         });
 
-        it.each([1], 'should contain the current element: %s', ['element'], function(/* element */){
+        it.each([1], 'should contain the current element: %s', ['element'], function (_element) {
             this._runnable.should.be.ok;
             this._runnable.title.should.be.ok;
             this._runnable.title.should.endWith('1');
         });
 
-        it.each([{ field: 1 }], 'should contain object values: %s', ['field'], function(/* element */){
+        it.each([{ field: 1 }], 'should contain object values: %s', ['field'], function (_element) {
             this._runnable.should.be.ok;
             this._runnable.title.should.be.ok;
             this._runnable.title.should.endWith('1');
         });
 
-        it.each([{ field: { inner: 1 } }], 'should contain nested object values: %s', ['field.inner'], function(/* element */){
+        it.each([{ field: { inner: 1 } }], 'should contain nested object values: %s', ['field.inner'], function (_element) {
             this._runnable.should.be.ok;
             this._runnable.title.should.be.ok;
             this._runnable.title.should.endWith('1');
         });
-
     });
-
 });
 
-describe('it.each with testPerIteration: false', function(){
-
+describe('it.each with testPerIteration: false', function () {
     require('../index')();
 
-    it.each([1], 'should be able to access the Mocha context', function(/* element */){
+    it.each([1], 'should be able to access the Mocha context', function (_element) {
         this.slow(1000);
         this.timeout(1000);
 
@@ -113,77 +104,78 @@ describe('it.each with testPerIteration: false', function(){
         this._runnable.parent._timeout.should.eql(2000);
     });
 
-    it.each(1, 'should place non-array values into an array', function(element){
+    it.each(1, 'should place non-array values into an array', function (element) {
         element.should.be.ok;
         element.should.eql(1);
     });
 
-    describe('executes', function(){
-
+    describe('executes', function () {
         this.slow(1000);
 
-        it.each([1], 'synchronously', function(/* element */){
+        it.each([1], 'synchronously', function (_element) {
             this._runnable.title.length.should.be.greaterThan(1);
         });
 
-        it.each([1], 'asynchronously', function(element, next){
+        it.each([1], 'asynchronously', function (_element, next) {
             this._runnable.title.length.should.be.greaterThan(1);
             setTimeout(next, 200);
         });
-
     });
 
-    describe('looping over arrays', function(){
-
+    describe('looping over arrays', function () {
         var count = 0;
 
-        it.each([1], 'should be able to access array elements', function(element){
+        it.each([1], 'should be able to access array elements', function (element) {
             element.should.be.ok;
             element.should.eql(1);
         });
 
-        it.each([1,2,3], 'should loop over each element in an array', function(element){
+        it.each([1, 2, 3], 'should loop over each element in an array', function (element) {
             element.should.be.ok;
             element.should.eql(++count);
         });
 
-        it('should increment a count', function(){
+        it('should increment a count', function () {
             count.should.be.ok;
             count.should.eql(3);
         });
-
     });
 
-    describe('titles', function(){
-
-        it.each([1], 'should contain the current index: %s', ['x'], function(/* element */){
+    describe('titles', function () {
+        it.each([1], 'should contain the current index: %s', ['x'], function (_element) {
             this._runnable.should.be.ok;
             this._runnable.title.should.be.ok;
             this._runnable.title.should.endWith('0');
         });
 
-        it.each([1], 'should contain the current element: %s', ['element'], function(/* element */){
+        it.each([1], 'should contain the current element: %s', ['element'], function (_element) {
             this._runnable.should.be.ok;
             this._runnable.title.should.be.ok;
             this._runnable.title.should.endWith('1');
         });
 
-        it.each([{ field: 1 }], 'should contain object values: %s', ['field'], function(/* element */){
+        it.each([{ field: 1 }], 'should contain object values: %s', ['field'], function (_element) {
             this._runnable.should.be.ok;
             this._runnable.title.should.be.ok;
             this._runnable.title.should.endWith('1');
         });
 
-        it.each([{ field: { inner: 1 } }], 'should contain nested object values: %s', ['field.inner'], function(/* element */){
+        it.each([{ field: { inner: 1 } }], 'should contain nested object values: %s', ['field.inner'], function (_element) {
             this._runnable.should.be.ok;
             this._runnable.title.should.be.ok;
             this._runnable.title.should.endWith('1');
         });
 
-        it.each([1,2], 'should contain iteration markers', function(element){
+        it.each([1,2], 'should contain iteration markers', function (element) {
             this._runnable.should.be.ok;
             this._runnable.title.should.be.ok;
             this._runnable.title.should.endWith('- ' + element + '/2');
+        });
+    });
+
+    describe('skipping', function () {
+        it.each.skip([2, 3, 4], 'should contain elements only less than 1', function (element) {
+            element.should.be.below(1);
         });
     });
 });
--- test/util.js
+++ test/util.js
@@ -2,33 +2,27 @@ var util = require('../lib/util');
 
 require('should');
 
-describe('Asynchronous Looping', function(){
-
-    it('should exit if a non-array value is provided', function(done){
+describe('Asynchronous Looping', function () {
+    it('should exit if a non-array value is provided', function (done) {
         var count = 0;
-
-        util.loop(1, function(element, next){
+        util.loop(1, function (_element, next) {
             count++;
             next();
-        }, function(){
+        }, function () {
             count.should.eql(0);
             done();
         });
-
     });
 
-    it('should pass errors up the call stack', function(done){
+    it('should pass errors up the call stack', function (done) {
         var count = 0;
-
-        util.loop([1,2,3], function(element, next){
+        util.loop([1,2,3], function (_element, next) {
             count++;
             next(new Error('My Error!'));
-        }, function(err){
+        }, function (err) {
             err.should.be.ok;
             count.should.eql(1);
             done();
         });
-
     });
-
 });
